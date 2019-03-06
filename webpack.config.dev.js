f9a803e0ca541776e39fe6d67560a16884a6f1fa
Vishesh Singh <visin@microsoft.com>
2019-03-06 08:04:03 +0530

    bundler is added

---
+++ webpack.config.dev.js
@@ -0,0 +1,22 @@
+import path from 'path';
+
+export default {
+  mode: "development",
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  plugins: [],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: ['babel-loader'] },
+      { test: /\.css$/, loader: ['style-loader', 'css-loader'] }
+    ]
+  }
+}
