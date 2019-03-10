a366c41ab28dc780406ee412dc4bdde2608103f2
Vishesh Singh <singhvishesh394@gmail.com>
2019-03-10 11:16:01 +0530

    appveyor.yml added

--- src/index.js
+++ src/index.js
@@ -4,3 +4,4 @@ import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 console.log(`I would pay ${courseValue} for this awesome course!`); // eslint-disable-line no-console
+console.log(`Program is working correctly`); // eslint-disable-line no-console
